c4c960cca1701349d6bb66d8f32d5e97b72a7db8
THDuc <DucTHGCS18081@FPT.Edu.vn>
2020-10-26 01:51:53 +0700

    backend

--- pages/api/brand/get-brand.js
+++ pages/api/brand/get-brand.js
@@ -1,7 +1,7 @@
 import Config from "../../../config/config";
 
 const Brand = (request, response) => {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+    // process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
     Config.query('select * from brand;', (err, res) => {
         if (err) {
             console.log(err)
